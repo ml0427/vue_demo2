67801a68e41558b831ec2d63a4c5887702c0420f
michaelNodeTest <ml0427@gmail.com>
2021-01-26 15:31:08 +0800

    demo

---
+++ demo/app.js
@@ -0,0 +1,12 @@
+const button1 = document.querySelector('button');
+const input1 = document.querySelector('input');
+const list1 = document.querySelector('ul');
+
+function addAPlan() {
+    const value = input1.value;
+    const item = document.createElement('li');
+    item.textContent = value;
+    list1.appendChild(item)
+}
+
+button1.addEventListener('click', addAPlan);
